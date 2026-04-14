a59ab5e71ee5b00b7c0e756325ffa86906cf6d2d
ajibon153 <ajimuhfauji95@@gmail.com>
2026-04-14 09:54:28 +0700

    remove prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,21 @@
 // https://docs.expo.dev/guides/using-eslint/
 module.exports = {
-  extends: ["expo", "prettier"],
-  plugins: ["prettier", "import"],
-  rules: {
-    "prettier/prettier": "error",
-    "import/order": [
-      "error",
-      {
-        groups: [
-          "builtin",
-          "external",
-          "internal",
-          "parent",
-          "sibling",
-          "index",
-          "object",
-          "type",
-        ],
-        "newlines-between": "always",
-        alphabetize: {
-          order: "asc",
-          caseInsensitive: true,
-        },
-      },
-    ],
-  },
-};
+    extends: ["expo", "prettier"],
+    plugins: ["prettier", "import"],
+    rules: {
+        "prettier/prettier": "error",
+        "import/order": [
+            "error",
+            {
+                singleQuote: true,
+                parser: "flow",
+                groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+                "newlines-between": "always",
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: true
+                }
+            }
+        ]
+    }
+}
