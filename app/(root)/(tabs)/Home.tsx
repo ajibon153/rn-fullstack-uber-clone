0e27694c1b837093ba8393fd78570b5f92702362
ajibon153 <ajimuhfauji95@@gmail.com>
2026-05-21 14:06:08 +0700

    custom components and page

---
+++ app/(root)/(tabs)/Home.tsx
@@ -0,0 +1,28 @@
+import { useUser } from "@clerk/expo"
+import { Text, View } from "react-native"
+
+export default function Page() {
+    const { isSignedIn, user, isLoaded } = useUser()
+
+    // Handle loading state
+    if (!isLoaded)
+        return (
+            <View>
+                <Text>Loading...</Text>
+            </View>
+        )
+
+    // Protect the page from unauthenticated users
+    if (!isSignedIn)
+        return (
+            <View>
+                <Text>Sign in to view this page</Text>
+            </View>
+        )
+
+    return (
+        <View>
+            <Text>Hello {user.id}!</Text>
+        </View>
+    )
+}
---
+++ app/_layout.tsx
@@ -0,0 +1,46 @@
+import { useFonts } from "expo-font"
+import { Stack } from "expo-router"
+import * as SplashScreen from "expo-splash-screen"
+
+import { ClerkProvider } from "@clerk/expo"
+import { tokenCache } from "@clerk/expo/token-cache"
+import { useEffect } from "react"
+import "./global.css"
+
+const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!
+
+const Layout = () => {
+    const [loaded] = useFonts({
+        "Jakarta-Bold": require("../assets/fonts/PlusJakartaSans-Bold.ttf"),
+        "Jakarta-ExtraBold": require("../assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+        "Jakarta-ExtraLight": require("../assets/fonts/PlusJakartaSans-ExtraLight.ttf"),
+        "Jakarta-Light": require("../assets/fonts/PlusJakartaSans-Light.ttf"),
+        "Jakarta-Medium": require("../assets/fonts/PlusJakartaSans-Medium.ttf"),
+        Jakarta: require("../assets/fonts/PlusJakartaSans-Regular.ttf"),
+        "Jakarta-SemiBold": require("../assets/fonts/PlusJakartaSans-SemiBold.ttf")
+    })
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync()
+        }
+        if (!publishableKey) {
+            throw new Error("Add your Clerk Publishable Key to the .env file")
+        }
+    }, [loaded])
+
+    return (
+        <ClerkProvider publishableKey={publishableKey} tokenCache={tokenCache}>
+            <Stack screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="welcome" />
+                <Stack.Screen name="sign-up" />
+                <Stack.Screen name="sign-in" />
+                <Stack.Screen name="index" />
+                <Stack.Screen name="(root)" />
+                <Stack.Screen name="(auth)" />
+            </Stack>
+        </ClerkProvider>
+    )
+}
+
+export default Layout
