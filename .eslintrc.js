95da272642c6fd731cc0904449f2d24268a41995
ajibon153 <ajimuhfauji95@@gmail.com>
2026-05-21 14:05:22 +0700

    rebase init project setting

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+    extends: ["expo", "prettier"],
+    plugins: ["prettier", "import"],
+    rules: {
+        "prettier/prettier": "error",
+        "import/order": [
+            "error",
+            {
+                singleQuote: true,
+                parser: "flow",
+                groups: ["builtin", "external", "internal", "parent", "sibling", "index", "object", "type"],
+                "newlines-between": "always",
+                alphabetize: {
+                    order: "asc",
+                    caseInsensitive: true
+                }
+            }
+        ]
+    }
+}
